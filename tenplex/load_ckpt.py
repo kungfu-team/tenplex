676411e663bc2654980ef28517fb3c6bef62d9b6
lg <lg4869@outlook.com>
2022-10-11 14:47:35 +0100

    init python package

---
+++ tenplex/load_ckpt.py
@@ -0,0 +1,84 @@
+import argparse
+import glob
+import os
+import pickle
+
+import numpy as np
+import torch
+
+from tensor_file import query_tensor_file, read_tensor_file, upload_tensor
+
+
+def construct_ckpt(path: str):
+    if os.path.isdir(path):
+        metadata_path = os.path.join(path, 'dir.meta')
+        if os.path.exists(metadata_path):
+            # dir has metadata
+            # dir is list
+            with open(metadata_path, 'r') as meta_fil:
+                metadata = meta_fil.readlines()
+            length = int(metadata[1])
+            if length == 0:
+                return []
+
+            lis = []
+            for i in range(length):
+                file_list = glob.glob(os.path.join(path, f'list{i:04d}*'))
+                file_list.sort()  # needs sorting for ndarray files
+                file_path = file_list[0]
+                ele = construct_ckpt(file_path)
+                lis.append(ele)
+
+            return lis
+
+        ckpt = {}
+        for entry in os.scandir(path):
+            name_split = entry.name.split('.', 1)
+            name = name_split[0]
+            try:
+                key = int(name)
+            except ValueError:
+                key = name
+            ckpt[key] = construct_ckpt(entry.path)
+
+        return ckpt
+
+    if os.path.isfile(path):
+        name = os.path.basename(path)
+        if name == 't0.txt':
+            return
+        if name.endswith('.meta'):
+            return
+
+        if name.endswith('.numpy.ndarray'):
+            tensor = read_tensor_file(path)
+            if 'np_rng_state' not in path:  # needs to stay numpy array
+                torch_tensor = torch.from_numpy(tensor)
+                return torch_tensor
+
+            return tensor
+
+        with open(path, 'rb') as fil:
+            byts = fil.read()
+
+        value = pickle.loads(byts)
+        if value == 'None':
+            value = None
+        return value
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Write checkpoint')
+    parser.add_argument('--mlfs-path', type=str)
+    args = parser.parse_args()
+
+    ckpt = construct_ckpt(args.mlfs_path)
+
+    torch.save(ckpt, './ckpt.pt')
+
+    #  import pprint
+    #  pprint.pprint(ckpt)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tenplex/save_ckpt.py
@@ -0,0 +1,120 @@
+import argparse
+import copy
+import pickle
+import re
+
+import numpy as np
+import requests
+import torch
+
+from tensor_file import query_tensor_file, read_tensor_file, upload_tensor
+
+
+def get_type(obj):
+    mat = re.match(r'<class \'(.*)\'>', str(type(obj)))
+    return mat.group(1)
+
+
+def upload_txt(path, txt):
+    host = '127.0.0.1'
+    ctrl_port = 20010
+
+    data = bytes(txt, 'utf-8')
+
+    headers = {
+        'Content-type': 'text/plain',
+    }
+    endpoint = f'http://{host}:{ctrl_port}/upload?path={path}'
+    r = requests.post(endpoint, headers=headers, data=data)
+    assert (r.status_code == 200)
+
+
+def upload_object(path, obj, typ=None):
+    host = '127.0.0.1'
+    ctrl_port = 20010
+
+    if typ is None:
+        typ = get_type(obj)
+    path = path + f'.{typ}'
+    data = pickle.dumps(obj)
+
+    headers = {
+        'Content-type': typ,
+    }
+    endpoint = f'http://{host}:{ctrl_port}/upload?path={path}'
+    r = requests.post(endpoint, headers=headers, data=data)
+    assert (r.status_code == 200)
+
+
+def add_values(vals, new_vals):
+    if isinstance(new_vals, list):
+        vals.extend(new_vals)
+    if isinstance(new_vals, tuple):
+        vals.append(new_vals)
+    return vals
+
+
+def traverse_ckpt(value, keys=None):
+    if keys is None:
+        keys = []
+
+    if isinstance(value, dict):
+        for key, val in value.items():
+            new_keys = copy.deepcopy(keys)
+            new_keys.append(key)
+            traverse_ckpt(val, new_keys)
+        return
+    if isinstance(value, (list, set, tuple)):
+        length = len(value)
+        metadata = f'list\n{length}'
+        str_keys = [str(k) for k in keys]
+        str_keys.append('dir')
+        base_path = '/'.join(str_keys)
+        txt_path = base_path + '.meta'
+        upload_txt(txt_path, metadata)
+
+        for i, val in enumerate(value):
+            new_keys = copy.deepcopy(keys)
+            new_keys.append(f'list{i:04d}')
+            traverse_ckpt(val, new_keys)
+        return
+
+    str_keys = [str(k) for k in keys]
+    base_path = '/'.join(str_keys)
+    if isinstance(value, torch.Tensor):
+        tensor = value.detach().numpy()
+        typ = get_type(tensor)
+        tensor_path = base_path + f'.{typ}'
+        upload_tensor(tensor_path, tensor)
+        return
+    if isinstance(value, np.ndarray):
+        print(f'{keys} is np.ndarray')
+        typ = get_type(value)
+        tensor_path = base_path + f'.{typ}'
+        upload_tensor(tensor_path, value)
+        return
+
+    if value is None:
+        value = 'None'
+        upload_object(base_path, value, 'none')
+        return
+
+    upload_object(base_path, value)
+
+
+def write_ckpt(args):
+    ckpt = torch.load(args.ckpt_path, map_location='cpu')
+    traverse_ckpt(ckpt)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Write checkpoint')
+    parser.add_argument('--mlfs-path', type=str)
+    parser.add_argument('--ckpt-path', type=str)
+    args = parser.parse_args()
+
+    write_ckpt(args)
+
+
+if __name__ == '__main__':
+    main()
