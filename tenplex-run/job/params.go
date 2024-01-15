7c5302c4d6632416c2570dd60d7c8b96c5b50b6d
Marcel Wagenländer <marcel.wagenlander19@imperial.ac.uk>
2024-01-15 21:02:09 +0000

    update paths

--- tenplex-run/job/params.go
+++ tenplex-run/job/params.go
@@ -182,7 +182,7 @@ func GenMegatronLMGPTCmd(c MDPConfig, rank int, jobID string, host string, jConf
 	args := []string{
 		`--save`, checkpoint_path,
 		`--load`, checkpoint_path,
-		`--data-path`, `/data/dataset/???`,
+		`--data-path`, `/data/dataset`,
 		`--tensor-model-parallel-size`, str(c.ModelParallelSize),
 		`--pipeline-model-parallel-size`, str(c.PipelineParallelSize),
 		// `--DDP-impl`, `local`,
