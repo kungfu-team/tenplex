9c2a9b6c265b90ca10b89462ea3561d9496ac99d
Marcel Wagenländer <marcel.wagenlander19@imperial.ac.uk>
2024-02-26 14:47:36 +0000

    use test scripts

--- benchmark/dynamic_resources/run.sh
+++ benchmark/dynamic_resources/run.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-scheduler_flags() {
+scheduler_common_flags() {
     echo -detect-self-ip ib0
-    # echo -reinstall
     echo -u $USER
 }
 
-tenplex-scheduler $(scheduler_flags) &
-pid=($!)
+scheduler_tenplex_flags() {
+    # echo -device-allocation ./tenplex-allocation.json
+    echo -device-allocation ./tenplex-allocation-test.json
+}
+
+scheduler_pytorch_flags() {
+    # echo -device-allocation ./pytorch-allocation.json
+    echo -device-allocation ./pytorch-allocation-test.json
+}
+
 
 join_() {
     local IFS=$1
@@ -18,21 +25,33 @@ join_() {
 list_hosts() {
     echo "10.10.10.1"
     echo "10.10.10.2"
-    echo "10.10.10.3"
-    echo "10.10.10.4"
+    # echo "10.10.10.3"
+    # echo "10.10.10.4"
 }
 
 hosts=$(join_ , $(list_hosts))
 
-user_flags() {
+user_common_flags() {
     echo -hosts $hosts
     echo -gpu-per-host 4
     echo -image kungfu.azurecr.io/mw-megatron-lm-23.06-update:latest
-    echo -plan ./single-job-time.json
     echo -timed-job
 }
 
-tenplex-user $(user_flags)
+user_tenplex_flags() {
+    # echo -plan ./tenplex-schedule.json
+    echo -plan ./tenplex-schedule-test.json
+}
 
-# Kill scheduler after user finishes
+user_pytorch_flags() {
+    # echo -plan ./pytorch-schedule.json
+    echo -plan ./pytorch-schedule-test.json
+}
+
+tenplex-scheduler $(scheduler_common_flags) $(scheduler_tenplex_flags) &
+tenplex-user $(user_common_flags) $(user_tenplex_flags)
+pkill -P $$
+
+tenplex-scheduler $(scheduler_common_flags) $(scheduler_pytorch_flags) &
+tenplex-user $(user_common_flags) $(user_pytorch_flags)
 pkill -P $$
