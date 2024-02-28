606f40c7454666e0685077f920b907c89aeab7c9
Marcel Wagenländer <marcel.wagenlander19@imperial.ac.uk>
2024-02-28 15:24:05 +0000

    add tenplex arg and save to npy

--- benchmark/dynamic_resources/read_tensorboard.py
+++ benchmark/dynamic_resources/read_tensorboard.py
@@ -1,6 +1,8 @@
-from tensorboard.backend.event_processing import event_accumulator
-import matplotlib.pyplot as plt
+import argparse
 import os
+import matplotlib.pyplot as plt
+from tensorboard.backend.event_processing import event_accumulator
+import numpy as np
 
 
 def load_metrics(tb_path):
@@ -14,13 +16,15 @@ def load_metrics(tb_path):
 
 
 def main():
-    tenplex = False
-    base_path = "/mnt/k1d2/ckpt"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--tenplex",
+                        action="store_true")
+    args = parser.parse_args()
 
-    if tenplex:
+    metrics = []
+    if args.tenplex:
         num_workers = 16
-        job_id = "pytorch-dyn-res"
-        metrics = []
+        job_id = "tenplex-dyn-res"
         for i in range(num_workers):
             tb_path = f"training/{job_id}/{i}/ckpt/tensorboard"
             if os.path.exists(tb_path):
@@ -34,6 +38,8 @@ def main():
     wall_times = [x[0] for x in metrics]
     steps = [x[1] for x in metrics]
     loss = [x[2] for x in metrics]
+
+    np.savez("loss.npz", wall_time=wall_times, step=steps, loss=loss)
 
     fig, axes = plt.subplots(2,1)
     axes[0].plot(wall_times, loss)
