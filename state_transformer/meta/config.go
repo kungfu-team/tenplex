679df1079f03c429a98b6bf17346db3b22583f14
lg <lg4869@outlook.com>
2024-02-28 15:35:10 +0000

    use structflag

--- state_transformer/meta/config.go
+++ state_transformer/meta/config.go
@@ -3,29 +3,29 @@ package meta
 import "github.com/kungfu-team/tenplex/tenplex-run/listflag"
 
 type Config struct {
-	CkptStructDir   string
-	SourceMPDegree  int
-	TargetMPDegree  int
-	SourcePPDegree  int
-	TargetPPDegree  int
-	SourceSize      int
-	TargetSize      int
+	CkptStructDir   string `flag:"ckpt-struct-dir"`
+	SourceMPDegree  int    `flag:"source-mp-degree"`
+	SourcePPDegree  int    `flag:"source-pp-degree"`
+	TargetMPDegree  int    `flag:"target-mp-degree"`
+	TargetPPDegree  int    `flag:"target-pp-degree"`
+	SourceSize      int    `flag:"source-size"`
+	TargetSize      int    `flag:"target-size"`
 	SourceDPDegree  int
 	TargetDPDegree  int
-	Precision       string
-	OutputTimestamp string
-	InputTimestamp  string
-	SourceHosts     listflag.Strings
-	TargetHosts     listflag.Strings
-	Port            int
-	GpusPerHost     int
-	MdpLibrary      string
-	SeqLength       int
-	JobID           string
-	NumLayers       int
-	Model           string
-	ModelSize       string
-	VocabSize       int
-	Step            int
+	Precision       string           `flag:"precision"`
+	InputTimestamp  string           `flag:"input-timestamp"`
+	OutputTimestamp string           `flag:"output-timestamp"`
+	SourceHosts     listflag.Strings `flag:"source-hosts"`
+	TargetHosts     listflag.Strings `flag:"target-hosts"`
+	Port            int              `flag:"port" default:"20010"`
+	GpusPerHost     int              `flag:"gpus-per-host" default:"4"`
+	MdpLibrary      string           `flag:"mdp-library"`
+	SeqLength       int              `flag:"sequence-length"`
+	JobID           string           `flag:"jobid" default:"0"`
+	NumLayers       int              `flag:"num-layers"`
+	Model           string           `flag:"model"`
+	ModelSize       string           `flag:"model-size"`
+	VocabSize       int              `flag:"vocab-size"`
+	Step            int              `flag:"step"`
 	Central         bool
 }
--- state_transformer/meta/flags.go
+++ state_transformer/meta/flags.go
@@ -2,34 +2,15 @@ package meta
 
 import (
 	"flag"
+
+	"github.com/kungfu-team/tenplex/tenplex-run/structflag"
 )
 
 func ReadFlags() (*Config, int) {
 	var conf Config
-	flag.StringVar(&conf.CkptStructDir, "ckpt-struct-dir", "", "Checkpoint structure directory")
-	flag.IntVar(&conf.SourcePPDegree, "source-pp-degree", 0, "Source PP degree")
-	flag.IntVar(&conf.TargetPPDegree, "target-pp-degree", 0, "Target PP degree")
-	flag.IntVar(&conf.SourceMPDegree, "source-mp-degree", 0, "Source MP degree")
-	flag.IntVar(&conf.TargetMPDegree, "target-mp-degree", 0, "Target MP degree")
-	flag.IntVar(&conf.SourceSize, "source-size", 0, "Source size")
-	flag.IntVar(&conf.TargetSize, "target-size", 0, "Target size")
-	flag.IntVar(&conf.SeqLength, "sequence-length", 0, "Sequence length")
-	flag.StringVar(&conf.Precision, "precision", "", "Precision")
-	flag.StringVar(&conf.InputTimestamp, "input-timestamp", "", "Input timestamp")
-	flag.StringVar(&conf.OutputTimestamp, "output-timestamp", "", "Output timestamp")
-	flag.StringVar(&conf.MdpLibrary, "mdp-library", "", "Multi-dimensional parallelism library")
-	flag.StringVar(&conf.Model, "model", "", "gpt or bert")
-	flag.StringVar(&conf.ModelSize, "model-size", "", "Model size")
-	flag.Var(&conf.SourceHosts, "source-hosts", "Host IPs separated by a comma")
-	flag.Var(&conf.TargetHosts, "target-hosts", "Host IPs separated by a comma")
-	flag.IntVar(&conf.Port, "port", 20010, "Control port")
-	flag.IntVar(&conf.GpusPerHost, "gpus-per-host", 4, "Control port")
+	structflag.RegisterFlags(&conf, flag.CommandLine)
 	var targetRank int
 	flag.IntVar(&targetRank, "target-rank", 0, "Target rank")
-	flag.StringVar(&conf.JobID, "jobid", "0", "Job ID")
-	flag.IntVar(&conf.NumLayers, "num-layers", 0, "Number of layers")
-	flag.IntVar(&conf.VocabSize, "vocab-size", 0, "Vocablulary size")
-	flag.IntVar(&conf.Step, "step", 0, "Step/Iteration")
 	flag.BoolVar(&conf.Central, "central", false, "")
 
 	flag.Parse()
