7edd28b5934bd62b41784a9fd0c5ea7de0b0ab94
lg <lg4869@outlook.com>
2024-04-09 12:10:11 +0000

    init t5

---
+++ benchmark/run-megatron-lm-t5.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+. $(dirname $0)/common.sh
+
+list_hosts() {
+    # echo "10.10.10.1"
+    echo "10.10.10.3"
+}
+
+flags() {
+    base_flags
+
+    echo -framework "megatron-lm"
+    echo -model "t5"
+    echo -model-size "large"
+    echo -dataset "enwiki"
+    echo -batch-size 128
+    echo -micro-batch-size 8
+    echo -precision "fp16"
+    echo -index-url "/data/megatron-lm/gpt-2/enwiki/npzs_seq1024/indices.txt"
+    echo -hosts "$(join $(list_hosts))"
+    echo -schedule-file "$(dirname $0)/schedule.json"
+    echo -gpu-per-host 4
+    echo -gpu-per-container 4
+    echo -seq-length 1024
+}
+
+tenplex-run $(flags)
+# >tenplex-run.log 2>&1
--- tenplex-run/job/params_t5.go
+++ tenplex-run/job/params_t5.go
@@ -8,7 +8,7 @@ func GenMegatronLMT5Cmd(c MDPConfig, rank int, jobID string, host string, jConf 
 	cmd = append(cmd, jConf.DistFlags(c, rank)...)
 	cmd = append(cmd, `/workspace/Megatron-LM/pretrain_t5.py`)
 
-	cmd = append(cmd, t5Args(jConf)...)
+	cmd = append(cmd, t5Args(c, jConf)...)
 	args := []string{
 		`--data-path`, `/data/dataset/gpt-2/my-gpt2_text_document`,
 		// `--DDP-impl`, `local`,
@@ -22,10 +22,26 @@ func GenMegatronLMT5Cmd(c MDPConfig, rank int, jobID string, host string, jConf 
 	return cmd
 }
 
-func t5Args(jConf *JobConfig) []string {
+func t5Args(c MDPConfig, jConf *JobConfig) []string {
 	t5_args := []string{
 		`--micro-batch-size`, str(jConf.MicroBatchSize),
 		`--global-batch-size`, str(jConf.BatchSize),
+		`--seq-length`, `1024`,
+		`--max-position-embeddings`, `1024`,
+		`--train-iters`, str(c.TrainIters),
+		`--lr-decay-iters`, `10000`,
+		`--lr-warmup-fraction`, `0.01`,
+		`--vocab-file`, `/workspace/Megatron-LM/vocab/gpt2-vocab.json`,
+		`--merge-file`, `/workspace/Megatron-LM/vocab/gpt2-merges.txt`,
+		`--data-impl`, `mmap`,
+		`--split`, `949,50,1`,
+		`--lr`, `0.00015`,
+		`--lr-decay-style`, `cosine`,
+		`--min-lr`, `1.0e-5`,
+		`--weight-decay`, `1e-2`,
+		`--clip-grad`, `1.0`,
+		`--lr-warmup-fraction`, `.01`,
+		`--kv-channels`, str(64),
 	}
 
 	return t5_args
