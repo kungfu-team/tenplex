0295b78e983c9820e90308849f8120c7343292f0
lg <lg4869@outlook.com>
2024-04-09 16:50:39 +0000

    cleanup

--- tenplex-run/job/params_bert.go
+++ tenplex-run/job/params_bert.go
@@ -8,21 +8,15 @@ func GenMegatronLMBERTCmd(c MDPConfig, rank int, jobID string, host string, jCon
 	}
 	cmd = append(cmd, jConf.DistFlags(c, rank)...)
 	cmd = append(cmd, `/workspace/Megatron-LM/pretrain_bert.py`)
-	var bert_args []string
 	var sizes = map[string]TransformerSize{
 		`base`:  TFSize(12, 768, 12),
 		`large`: TFSize(24, 1024, 16),
 	}
-	if ts, ok := sizes[jConf.ModelSize]; ok {
-		bert_args = append(bert_args, ts.ToPyArgs()...)
-	} else {
-		log.Fatalf("Model size not matching %s", jConf.ModelSize)
-	}
-	bert_args = append(bert_args, []string{
-		`--seq-length`, `1024`, // default: 512
-		`--max-position-embeddings`, `1024`, // default: 512
+	bert_args := []string{
+		`--seq-length`, str(1024), // default: 512
+		`--max-position-embeddings`, str(1024), // default: 512
 		`--lr`, `0.0001`,
-		`--lr-decay-iters`, `10000`,
+		`--lr-decay-iters`, str(10000),
 		`--train-iters`, str(c.TrainIters),
 		`--min-lr`, `0.00001`,
 		`--lr-warmup-fraction`, `0.01`,
@@ -32,7 +26,12 @@ func GenMegatronLMBERTCmd(c MDPConfig, rank int, jobID string, host string, jCon
 		`--split`, `949,50,1`,
 		`--data-path`, `/data/dataset/bert_text_sentence`,
 		`--distributed-backend`, `nccl`,
-	}...)
+	}
+	if ts, ok := sizes[jConf.ModelSize]; ok {
+		bert_args = append(bert_args, ts.ToPyArgs()...)
+	} else {
+		log.Fatalf("Model size not matching %s", jConf.ModelSize)
+	}
 	cmd = append(cmd, bert_args...)
 	cmd = append(cmd, jConf.LogFlags(c)...)
 	cmd = append(cmd, jConf.TenplexFlags(c, host)...)
