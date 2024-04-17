869dd75d7e9692266d4a9e2c4a0f983312e32133
lg <lg4869@outlook.com>
2024-04-17 16:50:35 +0100

    apply base_flags

--- benchmark/run-megatron-lm-gpt.sh
+++ benchmark/run-megatron-lm-gpt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-. ./common.sh
+. $(dirname $0)/common.sh
 
 list_hosts() {
     echo "10.10.10.1"
@@ -11,7 +11,8 @@ list_hosts() {
 }
 
 flags() {
-    echo -image "kungfu.azurecr.io/mw-megatron-lm-23.06-update:latest"
+    base_flags
+
     echo -framework "megatron-lm"
     echo -model "gpt"
     echo -model-size "large"
@@ -21,15 +22,11 @@ flags() {
     echo -precision "fp16"
     echo -index-url "/data/megatron-lm/gpt-2/enwiki/npzs_seq1024/indices.txt"
     echo -hosts "$(join $(list_hosts))"
-    echo -tenplex-prefix "$HOME/.tenplex"
     echo -schedule-file "$(dirname $0)/schedule.json"
     echo -para-config "$(dirname $0)/para-config.json"
-    echo -mlfs-port 20010
     echo -gpu-per-host 4
     echo -gpu-per-container 4
-    echo -user $USER
     echo -seq-length 1024
 }
 
-tenplex-run $(flags) > gpt.log 2>&1
-
+tenplex-run $(flags) >gpt.log 2>&1
