b19e594a1e343793cb815a8873662dc504b8ca7d
Marcel Wagenländer <marcel.wagenlander19@imperial.ac.uk>
2024-04-17 15:29:47 +0000

    adjust parameters for GPT experiment

--- benchmark/run-megatron-lm-gpt.sh
+++ benchmark/run-megatron-lm-gpt.sh
@@ -5,7 +5,9 @@ set -e
 
 list_hosts() {
     echo "10.10.10.1"
+    echo "10.10.10.2"
     echo "10.10.10.3"
+    echo "10.10.10.4"
 }
 
 flags() {
@@ -20,7 +22,8 @@ flags() {
     echo -index-url "/data/megatron-lm/gpt-2/enwiki/npzs_seq1024/indices.txt"
     echo -hosts "$(join $(list_hosts))"
     echo -tenplex-prefix "$HOME/.tenplex"
-    echo -schedule-file "$(pwd)/schedule.json"
+    echo -schedule-file "$(dirname $0)/schedule.json"
+    echo -para-config "$(dirname $0)/para-config.json"
     echo -mlfs-port 20010
     echo -gpu-per-host 4
     echo -gpu-per-container 4
@@ -28,5 +31,5 @@ flags() {
     echo -seq-length 1024
 }
 
-tenplex-run $(flags) > tenplex-run.log 2>&1
+tenplex-run $(flags) > gpt.log 2>&1
 
