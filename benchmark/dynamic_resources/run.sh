6217de00867ccd0a81f3ae14c3655f133bd94bb8
Marcel Wagenländer <marcel.wagenlander19@imperial.ac.uk>
2024-06-13 10:48:44 +0000

    add debugging

--- benchmark/dynamic_resources/run.sh
+++ benchmark/dynamic_resources/run.sh
@@ -4,7 +4,8 @@ set -e
 . $(pwd)/../common.sh
 
 hosts() {
-    cat $(pwd)/hosts.txt | grep -v '#'
+    # cat $(pwd)/hosts.txt | grep -v '#'
+    echo "10.10.10.2"
 }
 
 common_flags() {
@@ -27,13 +28,15 @@ common_flags() {
     echo -seed 1234
     echo -no-shuffle
 
-    echo -schedule-file "$(dirname $0)/schedule.json"
+    # echo -schedule-file "$(dirname $0)/schedule.json"
+    echo -schedule-file "$(dirname $0)/debug-schedule.json"
 }
 
 tenplex_flags() {
     common_flags
     echo -jobid dyn-res-ten
-    echo -para-config "$(dirname $0)/tenplex-para-config.json"
+    # echo -para-config "$(dirname $0)/tenplex-para-config.json"
+    echo -para-config "$(dirname $0)/debug-para-config.json"
 }
 
 tenplex_dp_flags() {
@@ -45,13 +48,14 @@ tenplex_dp_flags() {
 pytorch_flags() {
     common_flags
     echo -jobid dyn-res-pyt
-    echo -para-config "$(dirname $0)/pytorch-para-config.json"
+    # echo -para-config "$(dirname $0)/pytorch-para-config.json"
+    echo -para-config "$(dirname $0)/debug-para-config.json"
     echo -no-tenplex
 }
 
-tenplex-run $(tenplex_flags) >tenplex-dyn-res.log 2>&1
+# tenplex-run $(tenplex_flags) >tenplex-dyn-res.log 2>&1
 
 # tenplex-run $(tenplex_dp_flags) >tenplex-dp-dyn-res.log 2>&1
 
-# sudo rm -fr /mnt/k1d2/ckpt/*
-# tenplex-run $(pytorch_flags) >pytorch-dyn-res.log 2>&1
+sudo rm -fr /mnt/k1d2/ckpt/*
+tenplex-run $(pytorch_flags) >pytorch-dyn-res.log 2>&1
