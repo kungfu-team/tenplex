eef275b1901a72e57acb70a9cb3826ce6a1959d3
Marcel Wagenländer <marcel.wagenlander19@imperial.ac.uk>
2024-07-10 08:17:33 +0000

    add diff schedule for DP

--- benchmark/dynamic_resources/run.sh
+++ benchmark/dynamic_resources/run.sh
@@ -26,32 +26,33 @@ common_flags() {
     echo -detect-self-ip ib0
     echo -seed 1234
     echo -no-shuffle
-
-    echo -schedule-file "$(dirname $0)/schedule.json"
 }
 
 tenplex_flags() {
     common_flags
     echo -jobid dyn-res-ten
     echo -para-config "$(dirname $0)/tenplex-para-config.json"
+    echo -schedule-file "$(dirname $0)/tenplex-schedule.json"
 }
 
 tenplex_dp_flags() {
     common_flags
     echo -jobid dyn-res-tdp
     echo -para-config "$(dirname $0)/pytorch-para-config.json"
+    echo -schedule-file "$(dirname $0)/pytorch-schedule.json"
 }
 
 pytorch_flags() {
     common_flags
     echo -jobid dyn-res-pyt
     echo -para-config "$(dirname $0)/pytorch-para-config.json"
+    echo -schedule-file "$(dirname $0)/pytorch-schedule.json"
     echo -no-tenplex
 }
 
-tenplex-run $(tenplex_flags) >tenplex-dyn-res.log 2>&1
+# tenplex-run $(tenplex_flags) >tenplex-dyn-res.log 2>&1
 
-# tenplex-run $(tenplex_dp_flags) >tenplex-dp-dyn-res.log 2>&1
+tenplex-run $(tenplex_dp_flags) >tenplex-dp-dyn-res.log 2>&1
 
 # sudo rm -fr /mnt/k1d2/ckpt/*
 # tenplex-run $(pytorch_flags) >pytorch-dyn-res.log 2>&1
