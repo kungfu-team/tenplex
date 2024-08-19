9ff7b75e945207489486fc0fcc9f94a7cb3808c5
lg <lg4869@outlook.com>
2024-08-19 15:48:29 +0100

    fix script

--- mlfs/add-imagenet.sh
+++ mlfs/add-imagenet.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 set -e
 
-make
-
 flags() {
     echo -idx-name imagenet
-    echo -idx-file https://minddata.blob.core.windows.net/data/imagenet.idx.txt
+    echo -index-url https://tenplex.blob.core.windows.net/data/imagenet.idx.txt
     echo -ctrl-port 20000
 
     echo -progress 0
     echo -global-batch-size 23
-    echo -cluster-size 4
+    echo -dp-size 4
 
-    echo -fetch
-
-    echo -m 64
+    # echo -fetch
+    # echo -m 64
 }
 
-./bin/mlfs mount $(flags)
+mlfs mount $(flags)
