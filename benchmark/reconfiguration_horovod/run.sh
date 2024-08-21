2b94fecd2b1687434006ea2c0a9e00a355c4a484
lg <lg4869@outlook.com>
2024-08-21 11:56:54 +0000

    wip

--- benchmark/reconfiguration_horovod/run.sh
+++ benchmark/reconfiguration_horovod/run.sh
@@ -1,20 +1,7 @@
 #!/bin/sh
 set -e
 
-run() {
-    local np=$1
-    shift
-    horovodrun -np $np $@
-}
-
-train_flags_disk() {
-    echo --data-dir /data/imagenet/records
-}
-
-train_flags_tenplex() {
-    echo --mlfs-dir /mnt/mlfs
-    echo --job fig-13
-}
+cd $(dirname $0)
 
 with_log_file() {
     local filename=$1
@@ -22,6 +9,10 @@ with_log_file() {
     $@ | tee $filename
     echo "logged to $filename $ $@"
 }
+
+with_log_file 1.log ./with-docker horovodrun -np 2 python3 ./imagenet_resnet.py --data-dir /data/imagenet/records
+with_log_file 2.log ./with-docker horovodrun -np 2 python3 ./imagenet_resnet_horovod_elastic.py --data-dir /data/imagenet/records
+with_log_file 3.log ./with-docker horovodrun -np 2 python3 ./imagenet_resnet.py --mlfs-dir /mnt/mlfs --job fig-13
 
 # with_log_file 1.log run 2 python3 ./imagenet_resnet.py $(train_flags_disk)
 # with_log_file 3.log run 2 python3 ./imagenet_resnet_horovod_elastic.py $(train_flags_disk)
