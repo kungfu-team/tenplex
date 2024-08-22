1a0cd103a665450d6f5f9233525ddd1770ccc4ab
lg <lg4869@outlook.com>
2024-08-22 16:59:18 +0000

    add debug log

--- benchmark/reconfiguration_cluster_size/upgrade.sh
+++ benchmark/reconfiguration_cluster_size/upgrade.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
 set -e
 
-ansible-playbook -i hosts.txt ./tenplex.yml
+now_sec() {
+    date +%s
+}
 
-echo "pulling image"
-ansible-playbook -i hosts.txt ./tenplex-2.yml
+_show_duration() {
+    echo "$1s"
+}
+
+measure() {
+    echo "BEGIN $@"
+    local begin=$(now_sec)
+    $@
+    local end=$(now_sec)
+    local duration=$((end - begin))
+    echo "END $@, took $(_show_duration $duration)" | tee -a measure.log
+}
+
+upgrade_cluster() {
+    measure ansible-playbook -i hosts.txt ./tenplex.yml
+
+    measure ansible-playbook -i hosts.txt ./tenplex-2.yml
+}
+
+measure
