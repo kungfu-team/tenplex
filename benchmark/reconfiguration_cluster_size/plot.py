d1f63ab0e00e7f2804ee5f8ac466db6c74150eac
Marcel Wagenländer <marcel.wagenlander19@imperial.ac.uk>
2024-09-09 16:41:55 +0000

    add extract

--- benchmark/reconfiguration_cluster_size/plot.py
+++ benchmark/reconfiguration_cluster_size/plot.py
@@ -1,12 +1,45 @@
-import os
+import re
 
 import matplotlib.pyplot as plt
 
 
+def match_timestamp(pattern, lines):
+    for i, line in enumerate(lines):
+        mat = re.search(pattern, line)
+        if mat:
+            return float(mat.group(1)), i
+
+    return -1.0, -1
+
+
+def extract_times(paras, sizes):
+    reconf_times = {}
+    for para in paras:
+        reconf_times[para] = []
+        for size in sizes:
+            log_file_name = f"reconfig_{size}_{para}.log"
+
+            with open(log_file_name, "r", encoding="utf-8") as log_file:
+                lines = log_file.readlines()
+
+            pattern = r"Finished pretrain (\d+\.\d+)"
+            finish_timestamp, finish_line = match_timestamp(pattern, lines)
+            pattern = r"Start pretrain (\d+\.\d+)"
+            start_timestamp, _ = match_timestamp(pattern, lines[finish_line+1:])
+
+            latency = start_timestamp - finish_timestamp
+            reconf_times[para].append(latency)
+
+    return reconf_times
+
+
 def main():
     parallelisms = ["pp", "mp", "dp"]
+    sizes = [8, 16, 32]
     plt.rcParams["hatch.linewidth"] = 3
     linewidth = 2
+
+    reconf_times = extract_times(parallelisms, sizes)
 
     for par in parallelisms:
         plt.rc("figure", figsize=[8, 4.5])
@@ -15,18 +48,8 @@ def main():
         width = 0.3
 
         nd = ["4 to 8", "8 to 16", "16 to 32"]
-        print('TODO: extract data from log')
-        t_dp = [19.4, 25.7, 31.2]
-        t_pp = [11.3, 8.3, 6.6]
-        t_mp = [18.4, 10.7, 7.1]
-        if par == "dp":
-            t = t_dp
-        elif par == "pp":
-            t = t_pp
-        elif par == "mp":
-            t = t_mp
-        else:
-            return
+        t = reconf_times[par]
+
         ax.bar(
             nd,
             t,
