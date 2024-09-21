884c82d97e2a01c029bf48966c87db868b90fae6
Marcel Wagenländer <marwage@users.noreply.github.com>
2024-09-21 15:35:03 +0200

    Add numbers on top of the bars (#65)
    * add numbers on top of the bar
    
    * increase fontsize for numbers

--- benchmark/reconfiguration_horovod/plot.py
+++ benchmark/reconfiguration_horovod/plot.py
@@ -4,32 +4,34 @@ import pandas as pd
 
 
 def parse_log(filename):
-    lines = [l.strip() for l in open(filename) if 'Img/sec' in l]
+    lines = [l.strip() for l in open(filename) if "Img/sec" in l]
     s = lines[0]
-    parts = s.split(' ')
+    parts = s.split(" ")
     return float(parts[2])
 
 
-def parse_logs(output='throughput.csv'):
-    a = parse_log('1.log')
-    b = parse_log('2.log')
-    c = parse_log('3.log')
+def parse_logs(output="throughput.csv"):
+    a = parse_log("1.log")
+    b = parse_log("2.log")
+    c = parse_log("3.log")
     rows = [
-        ('name', 'throughput'),
-        ('Horovod', a),
-        ('Elastic', b),
-        ('Tenplex', c),
+        ("name", "throughput"),
+        ("Horovod", a),
+        ("Elastic", b),
+        ("Tenplex", c),
     ]
-    with open(output, 'w') as f:
+    with open(output, "w") as f:
         for r in rows:
-            f.write(','.join([str(x) for x in r]) + '\n')
+            f.write(",".join([str(x) for x in r]) + "\n")
 
 
 def plot():
-    sys = "Scalai"
+    sys = "Tenplex"
     plt.rcParams["hatch.linewidth"] = 3
     linewidth = 2
     width = 0.3
+    fontsize = 20
+    labelsize = 16
 
     data = pd.read_csv("./throughput.csv")
     data = data.replace(to_replace="Tenplex", value=sys)
@@ -47,11 +49,20 @@ def plot():
         linewidth=linewidth,
     )
 
-    fontsize = 20
-    labelsize = 16
+    for i, value in enumerate(data["throughput"]):
+        plt.text(
+            i,
+            value + 5,
+            str(value),
+            ha="center",
+            c="tab:blue",
+            fontsize=fontsize,
+        )
+
     ax.set_xlabel("System", fontsize=fontsize)
     ax.set_ylabel("Throughput (samples/s)", fontsize=fontsize)
     ax.tick_params(labelsize=labelsize)
+    ax.set_ylim(0, 500)
 
     fig.tight_layout()
     plt.savefig("./throughput.pdf")
